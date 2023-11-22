4b97e3718002b1728fad12464b1d83e35402c495
Anthony (Branch) <anthony@branchfurniture.com>
2023-11-22 12:46:09 +0100

    bah

--- app/schedules.js
+++ app/schedules.js
@@ -1,5 +1,7 @@
-export function eat_breakfast()
+module.exports.eat_breakfast = function()
 {
         console.log('Eating breakfast...');
         console.log('done.');
 }
+
+require('make-runnable');
